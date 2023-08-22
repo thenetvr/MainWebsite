71041654c2cf960704d20aaa82b7648111b6ad92
Noah Fajarda <noahfajarda@gmail.com>
2023-08-22 08:48:28 -0700

    added tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import reactLogo from "/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="flex justify-center">
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
