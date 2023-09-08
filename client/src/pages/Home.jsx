7718dd1e910d4642f263c217b9d0751b288661a2
Noah Fajarda <noahfajarda@gmail.com>
2023-09-08 01:54:40 -0700

    able to send email

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,10 +5,25 @@ import { motion } from "framer-motion";
 import { pageVariants } from "../utils/framerMotionVariants";
 // send email function
 import sendEmail from "../utils/sendEmail";
+import { useState } from "react";
 
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+
   // replace for any other pathname
   if (window.location.pathname !== "/") return window.location.replace("/");
+
+  // attempt to send email
+  const attemptEmail = () => {
+    if (!email.includes("@")) {
+      setError("INVALID EMAIL");
+      return;
+    }
+    setError("");
+    sendEmail(email);
+    setEmail("");
+  };
 
   return (
     <div className="bg-slate-800 h-max">
@@ -24,9 +39,15 @@ export default function Home() {
         </div>
         <div>
           <div>test</div>
-          <button className="bg-blue-800 p-3" onClick={sendEmail}>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <button className="bg-blue-800 p-3" onClick={attemptEmail}>
             send email
           </button>
+          <div className="text-red-500">{error}</div>
         </div>
       </motion.div>
       <Footer />
--- client/src/utils/sendEmail.js
+++ client/src/utils/sendEmail.js
@@ -1,6 +1,14 @@
-export default function sendEmail() {
+export default function sendEmail(email) {
   console.log("testinggg email")
-  fetch("/testing")
+  fetch("/testing", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email
+    })
+  })
     .then(res => res.json())
     .then(data => console.log(data))
 }
