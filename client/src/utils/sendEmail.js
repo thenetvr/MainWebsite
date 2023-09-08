80c1e4228a789271a61b79002ccef2a00d0e3b0a
Noah Fajarda <noahfajarda@gmail.com>
2023-09-08 00:51:50 -0700

    able to send email

--- client/src/utils/sendEmail.js
+++ client/src/utils/sendEmail.js
@@ -1,6 +1,14 @@
-export default function sendEmail() {
-  console.log("testinggg email")
-  fetch("/testing")
+export default function sendEmail(email) {
+  console.log(email)
+  fetch("/testing", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email
+    })
+  })
     .then(res => res.json())
     .then(data => console.log(data))
 }
--- server/routes/test.js
+++ server/routes/test.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const router = express.Router()
 
+// import nodemailer and create transport setup
+const nodemailer = require('nodemailer');
+const transporter = nodemailer.createTransport({
+  service: "hotmail",
+  auth: {
+    user: "instagram-clone-name@outlook.com",
+    pass: "Noahisthebest1!"
+  }
+})
+
 router.get("/", async (req, res) => {
   try {
     res.json({ "hello": "worlds" });
@@ -10,9 +20,25 @@ router.get("/", async (req, res) => {
   }
 })
 
-router.get("/testing", async (req, res) => {
+router.post("/testing", async (req, res) => {
   try {
-    res.json({ "new": "route" });
+    // NOTE: this might end up in spam
+    // send the email
+    transporter.sendMail({
+      // must match same email as transporter
+      from: "instagram-clone-name@outlook.com",
+      to: req.body.email,
+      subject: "This is Net VR!",
+      html: `<h1>Welcome To Net VR sir!</h1>
+      <br/>
+      <br/>
+      <p>We hope you enjoy using our platform!</p>
+      <br/>
+      <p>Sincerely, Net VR Team</p>
+      `, // html body
+    })
+
+    res.json({ message: "Saved Successfully. Check inbox or spam in your email!" })
   } catch (err) {
     res.status(500).send(err)
     console.error(err)
