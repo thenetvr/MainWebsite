79aa97e22305ea7ff0658f0f86d29ae73d20a198
Noah Fajarda <noahfajarda@gmail.com>
2023-09-19 14:20:02 -0700

    added contact us route to vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -13,7 +13,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/testing': routeConfig
+      '/testing': routeConfig,
+      '/contactUsMail': routeConfig,
     }
   }
 })
