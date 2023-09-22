38402fc9f9f5ad1fe3ac1b854391de51ac429d29
Yupeng-P <y_zheng10@u.pacific.edu>
2023-09-22 08:35:06 -0700

    News Branch
    News Branch

---
+++ client/src/utils/newsImages.jsx
@@ -0,0 +1,25 @@
+import Free_App from "../assets/Home-Theater1.png"
+import Gaming_Moment from "../assets/Home-Theater2.png"
+import Tech_Tribune from "../assets/Tech_Tribune.png"
+import Business_Journals from "../assets/The_Business_Journals.png"
+import Tech_Issue from "../assets/Tech_Issue.png"
+import Tie_OR from "../assets/Tie_Oregon.png"
+import GPU from "../assets/GPU.png"
+import GDC_Winner from "../assets/GDC_Winner.png"
+import Top_Steam from "../assets/Top_Steam_Games.png"
+import Launch_Day from "../assets/Launch_On_Steam.png"
+import Metaverse_Info from "../assets/Metaverse_Ecosystem.png"
+
+export default {
+  "img1": Free_App,
+  "img2": Gaming_Moment,
+  "img3": Tech_Tribune,
+  "img4": Business_Journals,
+  "img5": Tech_Issue,
+  "img6": Tie_OR,
+  "img7": GPU,
+  "img8": GDC_Winner,
+  "img9": Top_Steam,
+  "img10": Launch_Day,
+  "img11": Metaverse_Info
+}
